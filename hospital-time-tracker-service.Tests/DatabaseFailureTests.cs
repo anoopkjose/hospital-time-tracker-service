b8748e4aa6c0c569cafadc027b40947490b2a288
Anoop Jose <anoopkjose@gmail.com>
2025-08-16 12:04:30 +0530

    Updated CORS nd fixed errors

--- hospital-time-tracker-service.Tests/DatabaseFailureTests.cs
+++ hospital-time-tracker-service.Tests/DatabaseFailureTests.cs
@@ -9,31 +9,7 @@ namespace hospital_time_tracker_service.Tests
 {
     public class DatabaseFailureTests
     {
-        [Fact]
-        public async Task Scan_SaveChangesException_ReturnsInternalServerError()
-        {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
-
-            using var context = new HospitalContext(options);
-            var mockContext = new Mock<HospitalContext>(options);
-            mockContext.Setup(x => x.SaveChangesAsync(default))
-                .ThrowsAsync(new InvalidOperationException("Database error"));
-
-            var controller = new HospitalController(mockContext.Object);
-            var request = new ScanRequest { PatientId = "P001", Location = "main-entrance" };
-            var result = await controller.Scan(request);
-            var errorResult = Assert.IsType<ObjectResult>(result);
-            Assert.Equal(500, errorResult.StatusCode);
-        }
-
-        [Fact]
-        public async Task GetReports_QueryException_ReturnsInternalServerError()
-        {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-private DbContextOptions<HospitalContext> CreateDbContextOptions()
+        private DbContextOptions<HospitalContext> CreateDbContextOptions()
         {
             return new DbContextOptionsBuilder<HospitalContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
@@ -78,25 +54,6 @@ private DbContextOptions<HospitalContext> CreateDbContextOptions()
         public async Task GetAllVisits_QueryException_ReturnsInternalServerError()
         {
             var options = CreateDbContextOptions();
-
-            var mockContext = new Mock<HospitalContext>(options);
-            var mockDbSet = new Mock<DbSet<Visit>>();
-            mockDbSet.As<IQueryable<Visit>>().Setup(x => x.Provider)
-                .Throws(new InvalidOperationException("Database error"));
-            mockContext.Setup(x => x.Visits).Returns(mockDbSet.Object);
-
-            var controller = new HospitalController(mockContext.Object);
-            var result = await controller.GetReports(null, null);
-            var errorResult = Assert.IsType<ObjectResult>(result);
-            Assert.Equal(500, errorResult.StatusCode);
-        }
-
-        [Fact]
-        public async Task GetAllVisits_QueryException_ReturnsInternalServerError()
-        {
-            var options = new DbContextOptionsBuilder<HospitalContext>()
-                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-                .Options;
 
             var mockContext = new Mock<HospitalContext>(options);
             var mockDbSet = new Mock<DbSet<Visit>>();
